0df2f7fb494d3c4136e92dad535685c80a5afb19
BePsvPT <freedom@csie.io>
2017-06-04 17:36:43 +0800

    Fix front-end localization

--- resources/assets/js/i18n/data-table.js
+++ resources/assets/js/i18n/data-table.js
@@ -1,13 +1,25 @@
-const languages = ['en-US', 'zh-TW']
+const queryString = require('query-string')
+const store = require('store')
+
+const languages = ['en', 'zh-TW']
+const parsed = queryString.parse(location.search)
 
 let language = 'zh-TW'
 
-for (let lan of navigator.languages) {
-  if (languages.includes(lan)) {
-    language = lan
+if (parsed.lng && languages.includes(parsed.lng)) {
+  language = parsed.lng
+} else if (store.get('lng') && languages.includes(store.get('lng'))) {
+  language = store.get('lng')
+} else {
+  for (let lan of navigator.languages) {
+    if (languages.includes(lan)) {
+      language = lan
 
-    break
+      break
+    }
   }
 }
+
+store.set('lng', language)
 
 module.exports = require(`./data-table/${language}.json`)
